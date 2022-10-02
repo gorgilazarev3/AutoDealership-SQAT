086e904e65fe5a9f67d9b76d8aa8eaa077c51de1
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2022-10-02 22:09:22 +0200

    Made small changes to user management view

--- AutoDealership/Controllers/ManageController.cs
+++ AutoDealership/Controllers/ManageController.cs
@@ -74,7 +74,8 @@ namespace AutoDealership.Controllers
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                User = await UserManager.FindByIdAsync(userId)
             };
             return View(model);
         }
@@ -335,7 +336,8 @@ namespace AutoDealership.Controllers
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                User = await UserManager.FindByIdAsync(userId)
             };
             return PartialView("_ManageUserPanel", model);
         }
